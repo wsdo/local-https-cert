b1c2ee6e925a380d93db0dfcafd332898452e626
starkwang <wsd312@163.com>
2020-06-30 19:35:12 +0800

    feat: update config

--- createCertificate.sh
+++ createCertificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config server.csr.cnf
-openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 825 -sha256 -extfile v3.ext
+openssl req -new -sha256 -nodes -out devserver.csr -newkey rsa:2048 -keyout devserver.key -config server.csr.cnf
+openssl x509 -req -in devserver.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out devserver.crt -days 825 -sha256 -extfile v3.ext
